fb23fa8a8ac7b6f4a9aecade682603959efc68f2
Cody Mulkern <Codymulkern@gmail.com>
2016-04-22 01:27:04 -0400

    Graph procedures?
    Added some procedures to check average and stddev, also made a graph?
    Yay (apparently they use vectors that was fun trying to figure out)

--- cp1.rkt
+++ cp1.rkt
@@ -1,4 +1,7 @@
 #lang racket
+
+(require math/statistics)
+(require plot)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define nil '())
@@ -382,6 +385,103 @@
 
 ;; insert into database
 (insert-record-list retrieveData7)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Week 2
+;;
+;; (mean dealer)   ---> get the average of the dealers sale for the year
+;; (stddev dealer) ---> get standard deviation of the dealer for the year
+;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; -Find the average sold for the name and year
+;;
+;; --Once Ryan gets searching in DB working this procedure will be switched to (average-sold-for-year "Name of dealer" "Year")
+;;
+;; --Once Ryan gets searching in DB working this procedure will be switched to (stddev-sold-for-year "Name of dealer" "Year")
+;;
+;; --Need function to grab a entry of a specified name and year
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define (average-sold-for-year rec )
+  (if (null? rec) (error "Not Found") (printf "Average cars sold at ~a for the year ~a is: ~a" (first rec)
+                                                                                                  (second rec)
+                                                                                                  (mean (take (list-tail rec 2) 12))))
+)
+
+
+(define (stddev-sold-for-year rec )
+  (if (null? rec) (error "Not Found") (printf "Standard Deviation for cars sold at ~a for the year ~a is: ~a" (first rec)
+                                                                                                  (second rec)
+                                                                                                  (stddev (take (list-tail rec 2) 12))))
+)
+
+;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; -Plot a graph comparing two sets of data
+;;
+;; --Based on company and year
+;;
+;; --needs a helper function to create the vectors needed by discrete histogram procedure (make-vector rec)
+;;
+;; --Needed a regexp to get the names from the records
+;; ---(regexp-replace* #rx"([()])" (~a (list-tail (first rec) 1)) "")
+;;
+;; --Once Ryan gets searching in DB working this procedure will be switched to (vs name1 year1 name2 year2)
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define (make-vector rec)
+  (list (vector 'January (get-January-value2 rec))  (vector 'Feburary (get-February-value2  rec))  (vector 'March     (get-March-value2     rec))
+        (vector 'April   (get-April-value2   rec))  (vector 'May      (get-May-value2       rec))  (vector 'June      (get-June-value2      rec))
+        (vector 'July    (get-July-value2    rec))  (vector 'August   (get-August-value2    rec))  (vector 'September (get-September-value2 rec))
+        (vector 'October (get-October-value2 rec))  (vector 'November (get-November-value2  rec))  (vector 'December  (get-December-value2  rec))
+  )
+)
+
+(define (vs rec1 rec2)
+  (define v (list (vector 'January (get-January-value2 rec1)) (vector 'January (get-January-value2 rec1)) (vector 'January (get-January-value2 rec1))))
+  
+  (plot (list (discrete-histogram
+               ;;'(#(January (get-January-value2 rec1)) #(Feburary (get-February-value2 rec1)) #(March (get-March-value2 rec1)) #(April (get-April-value2 rec1)) #(May (get-May-value2 rec1)) #(June (get-June-value2 rec1))
+               ;;  #(July (get-July-value2 rec1)) #(August (get-August-value2 rec1)) #(September (get-September-value2 rec1)) #(October (get-October-value2 rec1)) #(November (get-November-value2 rec1)) #(December (get-December-value2 rec1)))
+               (make-vector rec1)
+               #:skip 2.5
+               #:x-min 0
+               #:y-max 1500
+               #:invert? #f
+               #:label (regexp-replace* #rx"([()])" (~a (list-tail (first rec1) 1)) ""))
+              (discrete-histogram
+              ;;'(#(January (get-January-value2 rec2)) #(Feburary (get-February-value2 rec2)) #(March (get-March-value2 rec2)) #(April (get-April-value2 rec2)) #(May (get-May-value2 rec2)) #(June (get-June-value2 rec2))
+              ;;  #(July (get-July-value2 rec2)) #(August (get-August-value2 rec2)) #(September (get-September-value2 rec2)) #(October (get-October-value2 rec2)) #(November (get-November-value2 rec2)) #(December (get-December-value2 rec2)))
+               (make-vector rec2)
+               #:skip 2.5
+               #:x-min 1
+               #:y-max 1500
+               #:invert? #f
+               #:label (regexp-replace* #rx"([()])" (~a (list-tail (first rec2) 1)) "") #:color 2 #:line-color 2))
+      #:width 1000
+      #:x-label "Month"
+      #:y-label "Sales"
+      #:out-file "totalSoldPlot.png"	 
+      #:out-kind 'png
+      )
+)
+
+               
+
+
+
+
+
+
 
 
 
